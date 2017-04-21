6bf573bc151378561427aa20045af19c659d200f
Irl Nathan <irlnathan@gmail.com>
2017-04-21 15:08:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# sails-webpack-react-server
+
+Slight modifications to [sails-webpack-seed](https://github.com/sailshq/sails-webpack-seed) to incorporate React as a server-rendered application.
+
+
+### Links
+
++ [Webpack main site](http://webpack.js.org)
++ [Sails framework documentation](http://sailsjs.com/documentation)
++ [Version notes / upgrading](http://sailsjs.com/documentation/upgrading/to-v-1-0)
++ [Deployment tips](http://sailsjs.com/documentation/concepts/deployment)
++ [Community support options](http://sailsjs.com/support)
++ [Sails Flagship](https://flagship.sailsjs.com)
---
+++ assets/js/homepage.js
@@ -0,0 +1,54 @@
+// var React = require('react');
+// var Component = require('react').Component;
+// var ReactDOM = require('react-dom');
+
+// You could use es6 module syntax instead
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// Require the styles for this page.  This will cause the `homepage.bundle.css` file
+// to be generated.  Note that you can require multiple .less or .css files here, and
+// they will _all_ be bundled together into `homepage.bundle.css` (the name is based
+// on the name of this JavaScript file).
+require('../styles/homepage.less');
+
+// Require the sockets.js file if you want to be able to use the socket client to
+// do things like `io.socket.get()` inside of this script.
+var io = require('../dependencies/sockets.js');
+
+// Using a class to create a component
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sails-React: Home Page</h1>
+        <a href='/about'>About</a>
+      </div>
+    );
+  }
+}
+
+// Using createClass to create Component
+// var SailsReact = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <h1>Hello Sails-React!</h1>
+//       </div>
+//     );
+//   }
+// });
+
+// Function React Component
+// const SailsReact = () => {
+//   return (
+//     <div>
+//       <h1>Hello Sails-React!</h1>
+//     </div>
+//   )
+// };
+
+ReactDOM.render(
+  <HomePage/>,
+  document.getElementById('homepage')
+);
